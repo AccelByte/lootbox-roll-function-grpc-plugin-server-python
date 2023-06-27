4fb084fc5bc0f1077d8d693d052d088921370402
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2023-06-27 13:35:18 +0700

    chore: add log grpc request response

--- src/app/services/lootbox_service.py
+++ src/app/services/lootbox_service.py
@@ -2,10 +2,12 @@
 # This is licensed software from AccelByte Inc, for limitations
 # and restrictions contact your company contract manager.
 
+import json
 import random
-
 from logging import Logger, getLogger
 from typing import List, Optional
+
+from google.protobuf.json_format import MessageToDict
 
 from app.proto.lootbox_pb2 import (
     RollLootBoxRewardsRequest,
@@ -21,27 +23,20 @@ class AsyncLootBoxService(LootBoxServicer):
     full_name: str = DESCRIPTOR.services_by_name["LootBox"].full_name
 
     def __init__(self, logger: Optional[Logger]) -> None:
-        self.logger = (
-            logger if logger is not None else getLogger(self.__class__.__name__)
-        )
+        self.logger = logger
             
     async def RollLootBoxRewards(self, request: RollLootBoxRewardsRequest, context):
-        self.logger.info("Received rollLootBoxRewards request")
-
+        self.log_payload(f'{self.RollLootBoxRewards.__name__} request: %s', request)
         final_items: List[RewardObject] = []
         rewards: List[LootBoxItemInfo.LootBoxRewardObject] = request.itemInfo.lootBoxRewards
         reward_weight_sum: int = 0
-
         reward_weight_sum = sum(reward.weight for reward in rewards)
-
         for i in range(request.quantity):
             for sel_idx in range(len(rewards)):
                 r = random.random() * reward_weight_sum
                 r -= rewards[sel_idx].weight
                 if r <= 0.0:
                     break
-            
-            
             sel_reward: LootBoxItemInfo.LootBoxRewardObject = rewards[sel_idx]
             item_count: int = len(sel_reward.items)
 
@@ -54,9 +49,15 @@ class AsyncLootBoxService(LootBoxServicer):
                 count=sel_item.count,
             )
             final_items.append(reward_item)
-        
         response: RollLootBoxRewardsResponse = RollLootBoxRewardsResponse(
             rewards=final_items
         )
-
+        self.log_payload(f'{self.RollLootBoxRewards.__name__} response: %s', response)
         return response
+    
+    def log_payload(self, format : str, payload):
+        if not self.logger:
+            return
+        payload_dict = MessageToDict(payload, preserving_proto_field_name=True)
+        payload_json = json.dumps(payload_dict)
+        self.logger.info(format % payload_json)
